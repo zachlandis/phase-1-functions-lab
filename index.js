045df9523cde451bb7d2bf599c63cd2fbf072c33
Zach <zachlandis91@gmail.com>
2022-12-12 14:31:23 -0700

    commit tree 2bc3202aa29bac69f666a4b81b8159d6e4656143

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num) {
+    const aboveHq = num - 42;
+    const absHq = Math.abs(aboveHq);
+
+    return absHq;
+}
+
+function distanceFromHqInFeet(num) {
+    const blocks = num - 42;
+    const feet = blocks * 264;
+    const absFeet = Math.abs(feet)
+
+    return absFeet;
+}
+
+
+function distanceTravelledInFeet(num1, num2) {
+    const blocks = num1 - num2;
+    const absBlocks = Math.abs(blocks);
+    const feet = absBlocks * 264;
+
+    return feet;
+}
+
+
+function calculatesFarePrice(num1, num2) {
+    const blocks = num1 - num2;
+    const absBlocks = Math.abs(blocks);
+    const feet = absBlocks * 264;
+
+    if (feet < 400) {
+        return 0;
+    } else if (feet >= 400 && feet <= 2000) {
+        return (feet - 400) * 0.02;
+    } else if (feet >= 2000 && feet <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far"
+    }
+
+}
